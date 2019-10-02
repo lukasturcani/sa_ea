b3f7719044707bcb9ef44c26920c4da132e8f951
Lukas Turcani <lukasturcani93@gmail.com>
2019-10-02 21:49:39 +0200

    use local stk

--- stages/run_ea/stages/run_ea/run_ea.bash
+++ stages/run_ea/stages/run_ea/run_ea.bash
@@ -1,8 +1,2 @@
-# Update stk and get the correct commit.
-cd /setup_environment/stk
-git fetch ea
-git checkout c92818290728b577856df51c7360e0c70271f767
-
-cd -
 source activate sa_ea_run_ea
-PYTHONPATH=/setup_environment/stk/src python -m stk.ea $1
+PYTHONPATH="$HOME/stk/src" python -m stk.ea $1
